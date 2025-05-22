d08ccea4157c9c00f10548130ce74ba8c4d595a7
myweb1 <119072006+myweb1@users.noreply.github.com>
2025-05-22 13:13:18 -0700

    I think it's complete idk

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 var a = 1
+var iwannaplusu = 1
+var Cost = 100
 var hoas = document.getElementById("hello")
+var fuku = document.getElementById("fuku")
 
 function Holafuncion() {
-  a += 1
-  hoas.innerHTML = a
+  a += iwannaplusu
+  hoas.innerHTML = "Holas:" + a
+}
+function upgrade() {
+  a -= Cost
+  hoas.innerHTML = "Holas:" + a
+  iwannaplusu + 1
+  Cost += Cost/2
+  fuku.innerHTML = "i'm a button that upgrades u and i have a cost of:" + Cost
 }
