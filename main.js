9e22189b22d7652ebee77bfcde810692dc34a4ba
myweb1 <119072006+myweb1@users.noreply.github.com>
2025-05-23 09:34:02 -0700

    ... just don't ask

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var a = 1
 var iwannaplusu = 1
+var immorelittleeverysecond
 var Cost = 100
+var seconds = 1000
 var hoas = document.getElementById("hello")
 var fuku = document.getElementById("fuku")
 
 function Holafuncion() {
   a += iwannaplusu
+  a +=
   hoas.innerHTML = "Holas:" + a
 }
 function upgrade() {
@@ -13,7 +16,16 @@ function upgrade() {
     a -= Cost
     hoas.innerHTML = "Holas:" + a
     iwannaplusu += 1
+    seconds += seconds/2
     Cost += Cost/2
    fuku.innerHTML = "i'm a button that upgrades u and i have a cost of:" + Cost
   }
+}
+function whyimdointhis() {
+  immorelittleeverysecond += iwannaplusu
+  
+}
+window.setInterval(secondfuk(),seconds)
+function secondfuk( ){ 
+  immorelittleeverysecond -= 1
 }
